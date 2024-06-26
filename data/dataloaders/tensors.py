d4155a6715da38e767fd197ed1d5266819e59169
camilobrownpinilla <camilobrownpinilla@gmail.com>
2024-06-26 10:23:49 -0400

    dataloading utilities

---
+++ data/dataloaders/tensors.py
@@ -0,0 +1,24 @@
+import torch
+
+def collate_tensors(batch):
+    dims = batch[0].dim()
+    max_size = [max([b.size(i) for b in batch]) for i in range(dims)]
+    size = (len(batch),) + tuple(max_size)
+    canvas = batch[0].new_zeros(size=size)
+    for i, b in enumerate(batch):
+        sub_tensor = canvas[i]
+        for d in range(dims):
+            sub_tensor = sub_tensor.narrow(d, 0, b.size(d))
+        sub_tensor.add_(b)
+    return canvas
+
+
+def collate(batch):
+    notnone_batches = [b for b in batch if b is not None]
+    databatch = [b['inp'] for b in notnone_batches]
+    
+    databatchTensor = collate_tensors(databatch)
+   
+    motion = databatchTensor
+
+    return motion
